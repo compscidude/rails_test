b1d3a7209d729366a6d4ee0a0770104d808eb0a9
compscidude <grahammeuller@hotmail.com>
2017-04-07 09:10:23 +0900

    add user edit, delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,6 @@ class UsersController < ApplicationController
     @users = User.all
   end 
   
-
-  
   def show
     @user = User.find(params[:id])  
   end 
@@ -14,6 +12,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end 
 
   def create
     @user = User.new(user_params)
@@ -25,6 +27,23 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    user = User.find(params[:id])
+    if(user.destroy)
+      flash[:success] = "Successfully Deleted User"
+    end 
+      redirect_to users_url
+  end 
+
+  def update
+    user = User.find(params[:id])
+    if(user.update_attributes(user_params))
+      flash[:success] = "Successfully Updated User"
+    end
+      redirect_to user_url
+  end 
+  
+
   private
 
   def user_params
