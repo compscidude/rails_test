b86267dd30e4c53d8b484903c07fd9fc723d1ca5
compscidude <grahammeuller@hotmail.com>
2017-04-07 14:13:05 +0900

    use before_action on certain actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   
 
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end 
@@ -44,6 +46,15 @@ class UsersController < ApplicationController
       redirect_to user_url
   end 
   
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+      
+  end
+  
+
 
   private
 
